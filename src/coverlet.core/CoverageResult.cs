b220394b5a6b15d5c5131d858dcd88e7df21c539
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-01-31 23:49:21 +0100

    add CoverageResult class

---
+++ src/coverlet.core/CoverageResult.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Coverlet.Core
+{
+    public class Lines : Dictionary<int, int> { }
+    public class Documents : Dictionary<string, Lines> { }
+    public class Data : Dictionary<string, Documents> { }
+
+    public class CoverageResult
+    {
+        public string Identifier;
+        public Data Data;
+    }
+}
