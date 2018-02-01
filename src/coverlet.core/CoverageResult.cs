54c07588cbd888b321f49e14fef6fe6dadb76fdf
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-02-01 23:58:37 +0100

    ensure lines in coverage results are sorted by line number

--- src/coverlet.core/CoverageResult.cs
+++ src/coverlet.core/CoverageResult.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Coverlet.Core
 {
-    public class Lines : Dictionary<int, int> { }
+    public class Lines : SortedDictionary<int, int> { }
     public class Documents : Dictionary<string, Lines> { }
     public class Data : Dictionary<string, Documents> { }
 
