f690e87936b24d34965c29d4a9423180c4b18229
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-02-10 10:39:42 +0100

    add tests for CoverageResult class

---
+++ test/coverlet.core.tests/CoverageResultTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using Moq;
+
+using Coverlet.Core;
+using Coverlet.Core.Reporters;
+
+namespace Coverlet.Core.Tests
+{
+    public class CoverageResultTests
+    {
+        [Fact]
+        public void TestFormat()
+        {
+            var mock = new Mock<IReporter>();
+
+            CoverageResult coverageResult = new CoverageResult();
+            coverageResult.Format(mock.Object);
+
+            mock.Verify(m => m.Format(coverageResult));
+        }
+    }
+}
