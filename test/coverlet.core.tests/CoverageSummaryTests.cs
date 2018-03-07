3bb6fd295e07a8be24137aabc0ab58ff963e772e
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-03-07 17:50:28 +0100

    add tests for CoverageSummary

---
+++ test/coverlet.core.tests/CoverageSummaryTests.cs
@@ -0,0 +1,32 @@
+using System;
+
+using Coverlet.Core;
+using Moq;
+using Xunit;
+
+namespace Coverlet.Core.Tests
+{
+    public class CoverageSummaryTests
+    {
+        [Fact]
+        public void TestCalculateSummary()
+        {
+            CoverageResult result = new CoverageResult();
+            result.Identifier = Guid.NewGuid().ToString();
+            Lines lines = new Lines();
+            lines.Add(1, 1);
+            lines.Add(2, 0);
+            Documents documents = new Documents();
+            documents.Add("doc.cs", lines);
+            result.Data = new Data();
+            result.Data.Add("module", documents);
+
+            CoverageSummary summary = new CoverageSummary(result);
+            CoverageSummaryResult summaryResult = summary.CalculateSummary();
+
+            Assert.NotEmpty(summaryResult);
+            Assert.True(summaryResult.ContainsKey("module"));
+            Assert.Equal(50, summaryResult["module"]);
+        }
+    }
+}
