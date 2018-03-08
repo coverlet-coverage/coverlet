a17070eeb94f8db1957370566e80cf6a212d81a1
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-03-08 23:09:46 +0100

    add coverlet console runner project

---
+++ src/coverlet.console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace coverlet.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Name = "coverlet";
+            app.FullName = "Cross platform .NET Core code coverage tool";
+            app.HelpOption("-h|--help");
+            app.VersionOption("-v|--version", GetAssemblyVersion());
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 0;
+            });
+        }
+
+        static string GetAssemblyVersion() => typeof(Program).Assembly.GetName().Version.ToString();
+    }
+}
