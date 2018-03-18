791660b832142bc2295fdd1f94ef8dafb8ba2771
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-03-18 02:17:04 +0100

    update tests to match changes in result structure

--- test/coverlet.core.tests/CoverageSummaryTests.cs
+++ test/coverlet.core.tests/CoverageSummaryTests.cs
@@ -16,8 +16,12 @@ namespace Coverlet.Core.Tests
             Lines lines = new Lines();
             lines.Add(1, 1);
             lines.Add(2, 0);
+            Methods methods = new Methods();
+            methods.Add("System.Void Coverlet.Core.Tests.CoverageSummaryTests.TestCalculateSummary()", lines);
+            Classes classes = new Classes();
+            classes.Add("Coverlet.Core.Tests.CoverageSummaryTests", methods);
             Documents documents = new Documents();
-            documents.Add("doc.cs", lines);
+            documents.Add("doc.cs", classes);
             result.Modules = new Modules();
             result.Modules.Add("module", documents);
 
--- test/coverlet.core.tests/Reporters/JsonReporterTests.cs
+++ test/coverlet.core.tests/Reporters/JsonReporterTests.cs
@@ -13,8 +13,12 @@ namespace Coverlet.Core.Reporters.Tests
             Lines lines = new Lines();
             lines.Add(1, 1);
             lines.Add(2, 0);
+            Methods methods = new Methods();
+            methods.Add("System.Void Coverlet.Core.Reporters.Tests.JsonReporterTests.TestFormat()", lines);
+            Classes classes = new Classes();
+            classes.Add("Coverlet.Core.Reporters.Tests.JsonReporterTests", methods);
             Documents documents = new Documents();
-            documents.Add("doc.cs", lines);
+            documents.Add("doc.cs", classes);
             result.Modules = new Modules();
             result.Modules.Add("module", documents);
 
