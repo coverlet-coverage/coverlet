4601c69616407a2c78f9c185820697099d31830a
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-03-31 14:48:25 +0300

    Refactor InstrumenterTests class - move creating temp directory to ctor of test class and implement IDisposable for clearing temp directory after running tests

--- test/coverlet.core.tests/Instrumentation/InstrumenterTests.cs
+++ test/coverlet.core.tests/Instrumentation/InstrumenterTests.cs
@@ -1,39 +1,51 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
-
+using Coverlet.Core.Attributes;
 using Xunit;
 using Coverlet.Core.Instrumentation;
 
 namespace Coverlet.Core.Instrumentation.Tests
 {
-    public class InstrumenterTests
+    public class InstrumenterTests : IDisposable
     {
-        [Fact]
-        public void TestInstrument()
+        private readonly string _module;
+        private readonly string _identifier;
+        private readonly DirectoryInfo _tempDirectory;
+
+        public InstrumenterTests()
         {
             //var module = typeof(InstrumenterTests).Assembly.Location;
             var module = typeof(Instrumenter).Assembly.Location;
-            
-            var pdb = Path.Combine(Path.GetDirectoryName(module), Path.GetFileNameWithoutExtension(module) + ".pdb");
-            var identifier = Guid.NewGuid().ToString();
 
-            var tempDirectoryPath = Path.Combine(Path.GetTempPath(), identifier);
-            var tempDirectory = Directory.CreateDirectory(tempDirectoryPath);
-            
-            var moduleFileForCopy = Path.Combine(tempDirectory.FullName, Path.GetFileName(module));
-            var pdbFileForCopy = Path.Combine(tempDirectory.FullName, Path.GetFileName(pdb));
+            var pdb = Path.Combine(Path.GetDirectoryName(module), Path.GetFileNameWithoutExtension(module) + ".pdb");
+            _identifier = Guid.NewGuid().ToString();
+
+            var tempDirectoryPath = Path.Combine(Path.GetTempPath(), _identifier);
+            _tempDirectory = Directory.CreateDirectory(tempDirectoryPath);
+
+            var moduleFileForCopy = Path.Combine(_tempDirectory.FullName, Path.GetFileName(module));
+            var pdbFileForCopy = Path.Combine(_tempDirectory.FullName, Path.GetFileName(pdb));
             File.Copy(module, moduleFileForCopy, true);
             File.Copy(pdb, pdbFileForCopy, true);
 
-            module = Path.Combine(tempDirectory.FullName, Path.GetFileName(module));
+            _module = Path.Combine(_tempDirectory.FullName, Path.GetFileName(module));
+        }
 
-            var instrumenter = new Instrumenter(module, identifier);
+        public void Dispose()
+        {
+            _tempDirectory.Delete(true);
+        }
+
+        [Fact]
+        public void TestInstrument()
+        {
+            var instrumenter = new Instrumenter(_module, _identifier);
             var result = instrumenter.Instrument();
 
-            Assert.Equal(Path.GetFileNameWithoutExtension(module), result.Module);
-            Assert.Equal(module, result.ModulePath);
-
-            tempDirectory.Delete(true);
+            Assert.Equal(Path.GetFileNameWithoutExtension(_module), result.Module);
+            Assert.Equal(_module, result.ModulePath);
         }
     }
 }
