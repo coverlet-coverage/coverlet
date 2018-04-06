4aa5d107d1efca2094d1861abf24c5fef0268638
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-04-06 13:30:50 +0300

    For unit tests changed existing ExcludeFromCoverage attribute so that it can be used on classes

--- src/coverlet.core/Attributes/ExcludeFromCoverage.cs
+++ src/coverlet.core/Attributes/ExcludeFromCoverage.cs
@@ -2,6 +2,6 @@ using System;
 
 namespace Coverlet.Core.Attributes
 {
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Class)]
     public class ExcludeFromCoverageAttribute : Attribute { }
 }
