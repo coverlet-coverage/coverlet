ce1482e54fd2d1cac7d8c3e975c0e05cae8cd4aa
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-04-06 13:37:44 +0300

    Was added 4 classes - 2 classes use custom ExcludeFromCoverage attribute, and 2 classes use ExcludeFromCodeCoverage attribute. Also 2 classes use exclude attribute only on nested methods, and 2 other classes are marked with exclude attrubute themselves

---
+++ test/coverlet.core.tests/Instrumentation/TestClasses/ClassWithExcludeFromCodeCoverageOnMethods.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Coverlet.Core.Instrumentation.Tests.TestClasses
+{
+    public class ClassWithExcludeFromCodeCoverageOnMethods
+    {
+        [ExcludeFromCodeCoverage]
+        public void MethodThatMustBeExcludedFromCoverage()
+        {
+
+        }
+        [ExcludeFromCodeCoverage]
+        public void MethodThatMustBeExcludedFromCoverage(int firstArgument)
+        {
+
+        }
+        [ExcludeFromCodeCoverage]
+        public void MethodThatMustBeExcludedFromCoverage(int firstArgument, string secondArgument)
+        {
+
+        }
+        [ExcludeFromCodeCoverage]
+        public void OtherMethodThatMustBeExcludedFromCoverage()
+        {
+
+        }
+
+        public void MethodThatMustBeIncludedToCoverage()
+        {
+
+        }
+        public void MethodThatMustBeIncludedToCoverage(int firstArgument)
+        {
+
+        }
+        public void MethodThatMustBeIncludedToCoverage(int firstArgument, string secondArgument)
+        {
+
+        }
+        [Description("Some descriotion 2")]
+        public void MethodThatMustBeIncludedToCoverageToo()
+        {
+
+        }
+        public void OtherMethodThatMustBeIncludedToCoverage()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        [ExcludeFromCodeCoverage]
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ test/coverlet.core.tests/Instrumentation/TestClasses/ClassWithExcludeFromCoverage.cs
@@ -0,0 +1,18 @@
+﻿using Coverlet.Core.Attributes;
+
+namespace Coverlet.Core.Instrumentation.Tests.TestClasses
+{
+    [ExcludeFromCoverage]
+    public class ClassWithExcludeFromCoverage
+    {
+        public void MethodThatMustBeExcludedFromCodeCoverage()
+        {
+
+        }
+
+        public void OtherMethodThatMustBeExcludedFromCodeCoverage()
+        {
+
+        }
+    }
+}
