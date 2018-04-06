8cc2a4acd6f5b432467bca4c94da3ed22233e7b8
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-04-06 14:01:06 +0300

    Added unit tests for StringExtensions. Change access modifier on class StringExtensions to internal. Change logic of method RemoveAttributeSuffix in StringExtensions class.

--- src/coverlet.core/Extensions/StringExtensions.cs
+++ src/coverlet.core/Extensions/StringExtensions.cs
@@ -1,10 +1,29 @@
-﻿namespace Coverlet.Core.Extensions
+﻿using System;
+
+namespace Coverlet.Core.Extensions
 {
-    public static class StringExtensions
+    internal static class StringExtensions
     {
         public static string RemoveAttributeSuffix(this string attributeName)
         {
-            return attributeName.Replace("Attribute", "");
+            var attributeSuffix = "Attribute";
+
+            if (IsSuffixPlacedAtTheEndOfWord(attributeName, attributeSuffix) &&
+                !IsWordContainsOnlySuffix(attributeName, attributeSuffix))
+            {
+                return attributeName.Replace(attributeSuffix, "");
+            }
+            return attributeName;
+        }
+
+        private static bool IsSuffixPlacedAtTheEndOfWord(string word, string suffix)
+        {
+            return word.LastIndexOf(suffix, StringComparison.Ordinal) == word.Length - suffix.Length;
+        }
+
+        private static bool IsWordContainsOnlySuffix(string word, string suffix)
+        {
+            return word == suffix;
         }
     }
 }
---
+++ test/coverlet.core.tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Coverlet.Core.Tests.Extensions
+{
+    [ExcludeFromCodeCoverage]
+    public class StringExtensionsTest
+    {
+        [Theory]
+        [InlineData("SomeAttribute")]
+        [InlineData("ExcludeFromCoverageAttribute")]
+        public void RemoveAttributeSuffix__CorrectlyRemoveAttributeSuffixInTheEndOfAttributeName(string attributeName)
+        {
+            // Arrange
+            // Act
+            var clearedAttributeName = Coverlet.Core.Extensions.StringExtensions.RemoveAttributeSuffix(attributeName);
+            // Assert
+            var attributeWord = "Attribute";
+            var attributeWordLength = attributeWord.Length;
+            var validClearedAttributeName =
+                attributeName.Remove(attributeName.LastIndexOf(attributeWord, StringComparison.Ordinal), attributeWordLength);
+            Assert.Equal(validClearedAttributeName, clearedAttributeName);
+        }
+
+        [Theory]
+        [InlineData("SomeAttributeWithSomeAdditionalSuffix")]
+        [InlineData("Attribute")]
+        public void RemoveAttributeSuffix__DoesNotRemoveAttributeSuffixIfItPlacedInMiddleOfWord(string word)
+        {
+            // Arrange
+            // Act
+            var clearedAttributeName = Coverlet.Core.Extensions.StringExtensions.RemoveAttributeSuffix(word);
+            // Assert
+            var validClearedAttributeName = word;
+            Assert.Equal(validClearedAttributeName, clearedAttributeName);
+        }
+    }
+}
