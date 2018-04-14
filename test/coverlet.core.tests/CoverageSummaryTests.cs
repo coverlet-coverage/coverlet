26538f8721761f05f6f76e7ef68c9bb50cfd9d18
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-04-14 16:52:43 +0100

    add tests for newly added coverage summary methods

--- test/coverlet.core.tests/CoverageSummaryTests.cs
+++ test/coverlet.core.tests/CoverageSummaryTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 using Coverlet.Core;
 using Moq;
@@ -8,29 +9,73 @@ namespace Coverlet.Core.Tests
 {
     public class CoverageSummaryTests
     {
-        [Fact]
-        public void TestCalculateSummary()
+        private Modules _modules;
+
+        public CoverageSummaryTests()
         {
-            CoverageResult result = new CoverageResult();
-            result.Identifier = Guid.NewGuid().ToString();
             Lines lines = new Lines();
-            lines.Add(1, new LineInfo { Hits = 1 });
+            lines.Add(1, new LineInfo { Hits = 1, IsBranchPoint = true });
             lines.Add(2, new LineInfo { Hits = 0 });
+
             Methods methods = new Methods();
-            methods.Add("System.Void Coverlet.Core.Tests.CoverageSummaryTests.TestCalculateSummary()", lines);
+            methods.Add("System.Void Coverlet.Core.Tests.CoverageSummaryTests::TestCalculateSummary()", lines);
+
             Classes classes = new Classes();
             classes.Add("Coverlet.Core.Tests.CoverageSummaryTests", methods);
+
             Documents documents = new Documents();
             documents.Add("doc.cs", classes);
-            result.Modules = new Modules();
-            result.Modules.Add("module", documents);
 
-            CoverageSummary summary = new CoverageSummary(result);
-            CoverageSummaryResult summaryResult = summary.CalculateSummary();
+            _modules = new Modules();
+            _modules.Add("module", documents);
+        }
 
-            Assert.NotEmpty(summaryResult);
-            Assert.True(summaryResult.ContainsKey("module"));
-            Assert.Equal(50, summaryResult["module"]);
+        [Fact]
+        public void TestCalculateLineCoverage()
+        {
+            CoverageSummary summary = new CoverageSummary();
+
+            var module = _modules.First();
+            var document = module.Value.First();
+            var @class = document.Value.First();
+            var method = @class.Value.First();
+            
+            Assert.Equal(0.5, summary.CalculateLineCoverage(module));
+            Assert.Equal(0.5, summary.CalculateLineCoverage(document));
+            Assert.Equal(0.5, summary.CalculateLineCoverage(@class));
+            Assert.Equal(0.5, summary.CalculateLineCoverage(method));
+        }
+
+        [Fact]
+        public void TestCalculateBranchCoverage()
+        {
+            CoverageSummary summary = new CoverageSummary();
+
+            var module = _modules.First();
+            var document = module.Value.First();
+            var @class = document.Value.First();
+            var method = @class.Value.First();
+
+            Assert.Equal(1, summary.CalculateBranchCoverage(module));
+            Assert.Equal(1, summary.CalculateBranchCoverage(document));
+            Assert.Equal(1, summary.CalculateBranchCoverage(@class));
+            Assert.Equal(1, summary.CalculateBranchCoverage(method));
+        }
+
+        [Fact]
+        public void TestCalculateMethodCoverage()
+        {
+            CoverageSummary summary = new CoverageSummary();
+
+            var module = _modules.First();
+            var document = module.Value.First();
+            var @class = document.Value.First();
+            var method = @class.Value.First();
+
+            Assert.Equal(1, summary.CalculateMethodCoverage(module));
+            Assert.Equal(1, summary.CalculateMethodCoverage(document));
+            Assert.Equal(1, summary.CalculateMethodCoverage(@class));
+            Assert.Equal(1, summary.CalculateMethodCoverage(method));
         }
     }
 }
