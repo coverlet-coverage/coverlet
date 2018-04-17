fb5f20fc571f82a0449c7f8be624721fa83a1fe3
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-04-17 23:04:15 +0100

    fix cobertura report generator output

--- src/coverlet.core/Reporters/CoberturaReporter.cs
+++ src/coverlet.core/Reporters/CoberturaReporter.cs
@@ -18,6 +18,8 @@ namespace Coverlet.Core.Reporters
         {
             CoverageSummary summary = new CoverageSummary();
 
+            int totalLines = 0, coveredLines = 0, totalBranches = 0, coveredBranches = 0;
+
             XDocument xml = new XDocument();
             XElement coverage = new XElement("coverage");
             coverage.Add(new XAttribute("line-rate", summary.CalculateLineCoverage(result.Modules).ToString()));
@@ -26,16 +28,14 @@ namespace Coverlet.Core.Reporters
             coverage.Add(new XAttribute("timestamp", ((int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds).ToString()));
 
             XElement sources = new XElement("sources");
-            foreach (var src in GetSources(result.Modules))
-            {
-                XElement source = new XElement("source", src);
-                sources.Add(source);
-            }
+            var basePath = GetBasePath(result.Modules);
+            sources.Add(new XElement("source", basePath));
 
             XElement packages = new XElement("packages");
             foreach (var module in result.Modules)
             {
                 XElement package = new XElement("package");
+                package.Add(new XAttribute("name", Path.GetFileNameWithoutExtension(module.Key)));
                 package.Add(new XAttribute("line-rate", summary.CalculateLineCoverage(module.Value).ToString()));
                 package.Add(new XAttribute("branch-rate", summary.CalculateBranchCoverage(module.Value).ToString()));
                 package.Add(new XAttribute("complexity", "0"));
@@ -47,17 +47,19 @@ namespace Coverlet.Core.Reporters
                     {
                         XElement @class = new XElement("class");
                         @class.Add(new XAttribute("name", cls.Key));
-                        @class.Add(new XAttribute("filename", Path.GetFileName(document.Key)));
+                        @class.Add(new XAttribute("filename", GetRelativePathFromBase(basePath, document.Key)));
                         @class.Add(new XAttribute("line-rate", summary.CalculateLineCoverage(cls.Value).ToString()));
                         @class.Add(new XAttribute("branch-rate", summary.CalculateBranchCoverage(cls.Value).ToString()));
                         @class.Add(new XAttribute("complexity", "0"));
 
+                        XElement classLines = new XElement("lines");
                         XElement methods = new XElement("methods");
+
                         foreach (var meth in cls.Value)
                         {
                             XElement method = new XElement("method");
                             method.Add(new XAttribute("name", meth.Key.Split(':')[2].Split('(')[0]));
-                            method.Add(new XAttribute("signature", meth.Key));
+                            method.Add(new XAttribute("signature", "(" + meth.Key.Split(':')[2].Split('(')[1]));
                             method.Add(new XAttribute("line-rate", summary.CalculateLineCoverage(meth.Value).ToString()));
                             method.Add(new XAttribute("branch-rate", summary.CalculateBranchCoverage(meth.Value).ToString()));
 
@@ -69,7 +71,29 @@ namespace Coverlet.Core.Reporters
                                 line.Add(new XAttribute("hits", ln.Value.Hits.ToString()));
                                 line.Add(new XAttribute("branch", ln.Value.IsBranchPoint.ToString()));
 
+                                totalLines++;
+                                if (ln.Value.Hits > 0) coveredLines++;
+
+
+                                if (ln.Value.IsBranchPoint)
+                                {
+                                    line.Add(new XAttribute("condition-coverage", "100% (1/1)"));
+                                    XElement conditions = new XElement("conditions");
+                                    XElement condition = new XElement("condition");
+                                    condition.Add(new XAttribute("number", "0"));
+                                    condition.Add(new XAttribute("type", "jump"));
+                                    condition.Add(new XAttribute("coverage", "100%"));
+
+                                    totalBranches++;
+                                    if (ln.Value.Hits > 0) coveredBranches++;
+
+                                    conditions.Add(condition);
+                                    line.Add(conditions);
+                                }
+
+
                                 lines.Add(line);
+                                classLines.Add(line);
                             }
 
                             method.Add(lines);
@@ -77,6 +101,7 @@ namespace Coverlet.Core.Reporters
                         }
 
                         @class.Add(methods);
+                        @class.Add(classLines);
                         classes.Add(@class);
                     }
                 }
@@ -84,6 +109,11 @@ namespace Coverlet.Core.Reporters
                 package.Add(classes);
                 packages.Add(package);
             }
+
+            coverage.Add(new XAttribute("lines-covered", coveredLines.ToString()));
+            coverage.Add(new XAttribute("lines-valid", totalLines.ToString()));
+            coverage.Add(new XAttribute("branches-covered", coveredBranches.ToString()));
+            coverage.Add(new XAttribute("branches-valid", totalBranches.ToString()));
 
             coverage.Add(sources);
             coverage.Add(packages);
@@ -95,16 +125,33 @@ namespace Coverlet.Core.Reporters
             return Encoding.UTF8.GetString(stream.ToArray());
         }
 
-        private string[] GetSources(Modules modules)
+        private string GetBasePath(Modules modules)
         {
             List<string> sources = new List<string>();
+            string source = string.Empty;
+
             foreach (var module in modules)
             {
                 sources.AddRange(
                     module.Value.Select(d => Path.GetDirectoryName(d.Key)));
             }
 
-            return sources.Distinct().ToArray();
+            sources = sources.Distinct().ToList();
+            var segments = sources[0].Split(Path.DirectorySeparatorChar);
+
+            foreach (var segment in segments)
+            {
+                var startsWith = sources.All(s => s.StartsWith(source + segment));
+                if (!startsWith)
+                    break;
+
+                source += segment + Path.DirectorySeparatorChar;
+            }
+
+            return source;
         }
+
+        private string GetRelativePathFromBase(string source, string path)
+            => path.Replace(source, string.Empty);
     }
 }
