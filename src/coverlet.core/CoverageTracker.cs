686394d7ec03f2c3bd6f234bd9ebf1f4a4a9c962
Jason Hunter <hunterjm@gmail.com>
2018-05-01 21:33:17 -0400

    save on disk space by compressing the hits file

--- src/coverlet.core/CoverageTracker.cs
+++ src/coverlet.core/CoverageTracker.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.IO.Compression;
 using Coverlet.Core.Attributes;
 using Coverlet.Core.Extensions;
 
@@ -10,11 +10,13 @@ namespace Coverlet.Core
     public static class CoverageTracker
     {
         private static Dictionary<string, List<string>> _markers;
+        private static Dictionary<string, int> _markerFileCount;
 
         [ExcludeFromCoverage]
         static CoverageTracker()
         {
             _markers = new Dictionary<string, List<string>>();
+            _markerFileCount = new Dictionary<string, int>();
             AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
         }
 
@@ -25,10 +27,20 @@ namespace Coverlet.Core
             {
                 _markers.TryAdd(path, new List<string>());
                 _markers[path].Add(marker);
+                _markerFileCount.TryAdd(path, 0);
                 if (_markers[path].Count >= 100000)
                 {
-                    File.AppendAllLines(path, _markers[path]);
+                    using (var fs = new FileStream($"{path}_compressed_{_markerFileCount[path]}", FileMode.OpenOrCreate))
+                    using (var gz = new GZipStream(fs, CompressionMode.Compress))
+                    using (var sw = new StreamWriter(gz))
+                    {
+                        foreach(var line in _markers[path])
+                        {
+                            sw.WriteLine(line);
+                        }
+                    }
                     _markers[path].Clear();
+                    _markerFileCount[path] = _markerFileCount[path] + 1;
                 }
             }
         }
@@ -37,7 +49,18 @@ namespace Coverlet.Core
         public static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
             foreach (var kvp in _markers)
-                File.AppendAllLines(kvp.Key, kvp.Value);
+            {
+                _markerFileCount.TryAdd(kvp.Key, 0);
+                using (var fs = new FileStream($"{kvp.Key}_compressed_{_markerFileCount[kvp.Key]}", FileMode.OpenOrCreate))
+                using (var gz = new GZipStream(fs, CompressionMode.Compress))
+                using (var sw = new StreamWriter(gz))
+                {
+                    foreach(var line in kvp.Value)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+            }
         }
     }
 }
