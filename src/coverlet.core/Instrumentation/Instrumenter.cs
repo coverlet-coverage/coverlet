6f2fc30a012e6be44d923aaf9439f416b12c2ce8
Coen Munckhof <c.munckhof@gmail.com>
2018-05-21 21:51:28 +0200

    Extended the InstrumenterFactory to decorate the real instance with  all registered decorators. Added EnableLogging method that registers the logging decorator to the InstrumenterFactory

--- src/coverlet.core/Instrumentation/Instrumenter.cs
+++ src/coverlet.core/Instrumentation/Instrumenter.cs
@@ -23,12 +23,25 @@ namespace Coverlet.Core.Instrumentation
 
     internal static class InstrumenterFactory
     {
+        private static readonly List<Func<IInstrumenter, IInstrumenter>>  DecoratorFunctions = new List<Func<IInstrumenter, IInstrumenter>>();
 
         public static IInstrumenter Create(string module, string identifier, string[] filters, string[] excludedFiles)
         {
             IInstrumenter result = new Instrumenter(module, identifier, filters, excludedFiles);
 
+            // Decorate the real Instrumenter instance
+            foreach (var decoratorFunc in DecoratorFunctions)
+                result = decoratorFunc.Invoke(result);
+
             return result;
+        }
+
+        public static void RegisterDecorator(Func<IInstrumenter, IInstrumenter> decoratingFunc)
+        {
+            if (decoratingFunc == null)
+                throw new ArgumentNullException(nameof(decoratingFunc));
+
+            DecoratorFunctions.Add(decoratingFunc);
         }
     }
 
---
+++ src/coverlet.core/Logging/EnableLogging.cs
@@ -0,0 +1,14 @@
+﻿using coverlet.core.Logging.Decorators;
+using Coverlet.Core.Instrumentation;
+using Coverlet.Core.Logging;
+
+namespace coverlet.core.Logging
+{
+    public static class EnableLogging
+    {
+        public static void Execute()
+        {
+            InstrumenterFactory.RegisterDecorator(instrumenter => new InstrumenterLoggerDecorator(instrumenter, LoggerFactory.GetLogger()));
+        }
+    }
+}
