121ce322bd4375012fde1abc87d432ec3a05be0b
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2018-07-06 16:51:31 -0700

    Enhance the tracking perf

--- src/coverlet.tracker/CoverageTracker.cs
+++ src/coverlet.tracker/CoverageTracker.cs
@@ -7,12 +7,15 @@ namespace Coverlet.Tracker
 {
     public static class CoverageTracker
     {
-        private static Dictionary<string, Dictionary<string, int>> _events;
+        private static List<Dictionary<string, Dictionary<string, int>>> _events;
+
+        [ThreadStatic]
+        private static Dictionary<string, Dictionary<string, int>> t_events;
 
         [ExcludeFromCodeCoverage]
         static CoverageTracker()
         {
-            _events = new Dictionary<string, Dictionary<string, int>>();
+            _events = new List<Dictionary<string, Dictionary<string, int>>>();
             AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
             AppDomain.CurrentDomain.DomainUnload += new EventHandler(CurrentDomain_ProcessExit);
         }
@@ -20,12 +23,23 @@ namespace Coverlet.Tracker
         [ExcludeFromCodeCoverage]
         public static void MarkExecuted(string file, string evt)
         {
-            lock (_events)
+            if (t_events == null)
             {
-                if (!_events.TryGetValue(file, out var fileEvents))
+                t_events = new Dictionary<string, Dictionary<string, int>>();
+                lock (_events)
+                {
+                    _events.Add(t_events);
+                }
+            }
+
+            // We are taking the lock only for synchronizing with CurrentDomain_ProcessExit event
+            // but the lock should be fast as the thread shouldn't block before getting process exit event
+            lock (t_events)
+            {
+                if (!t_events.TryGetValue(file, out var fileEvents))
                 {
                     fileEvents = new Dictionary<string, int>();
-                    _events.Add(file, fileEvents);
+                    t_events.Add(file, fileEvents);
                 }
 
                 if (!fileEvents.TryGetValue(evt, out var count))
@@ -42,16 +56,30 @@ namespace Coverlet.Tracker
         [ExcludeFromCodeCoverage]
         public static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
+            Dictionary<string, bool> encounteredFile = new Dictionary<string, bool>();
+
             lock (_events)
             {
-                foreach (var files in _events)
+                foreach (var localEvents in _events)
                 {
-                    using (var fs = new FileStream(files.Key, FileMode.Create))
-                    using (var sw = new StreamWriter(fs))
+                    lock (localEvents)
                     {
-                        foreach (var evt in files.Value)
+                        foreach (var files in localEvents)
                         {
-                            sw.WriteLine($"{evt.Key},{evt.Value}");
+                            FileMode mode = FileMode.Open;
+                            if (!encounteredFile.TryGetValue(files.Key, out bool b))
+                            {
+                                encounteredFile.Add(files.Key, true);
+                                mode = FileMode.Create;
+                            }
+                            using (var fs = new FileStream(files.Key, mode))
+                            using (var sw = new StreamWriter(fs))
+                            {
+                                foreach (var evt in files.Value)
+                                {
+                                    sw.WriteLine($"{evt.Key},{evt.Value}");
+                                }
+                            }
                         }
                     }
                 }
