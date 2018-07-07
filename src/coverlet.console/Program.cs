e8379f3d58c14b8c523ff86bdcf204504b954054
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-07-07 20:45:38 +0100

    update cli options to match msbuild task

--- src/coverlet.console/Program.cs
+++ src/coverlet.console/Program.cs
@@ -1,55 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+
+using Coverlet.Console.Logging;
+using Coverlet.Core;
+using Coverlet.Core.Reporters;
 
 using Microsoft.Extensions.CommandLineUtils;
 
-namespace coverlet.console
+namespace Coverlet.Console
 {
     class Program
     {
         static int Main(string[] args)
         {
+            var logger = new ConsoleLogger();
             var app = new CommandLineApplication();
             app.Name = "coverlet";
             app.FullName = "Cross platform .NET Core code coverage tool";
             app.HelpOption("-h|--help");
             app.VersionOption("-v|--version", GetAssemblyVersion());
 
-            CommandArgument project = app.Argument("<PROJECT>", "The project to test. Defaults to the current directory.");
-            CommandOption config = app.Option("-c|--configuration", "Configuration to use for building the project.", CommandOptionType.SingleValue);
-            CommandOption output = app.Option("-o|--coverage-output", "The output path of the generated coverage report", CommandOptionType.SingleValue);
-            CommandOption format = app.Option("-f|--coverage-format", "The format of the coverage report", CommandOptionType.SingleValue);
+            CommandArgument module = app.Argument("<ASSEMBLY>", "Path to the test assembly.");
+            CommandOption target = app.Option("-t|--target", "Path to the test runner application.", CommandOptionType.SingleValue);
+            CommandOption targs = app.Option("-a|--targetargs", "Arguments to be passed to the test runner.", CommandOptionType.SingleValue);
+            CommandOption output = app.Option("-o|--output", "Output of the generated coverage report", CommandOptionType.SingleValue);
+            CommandOption format = app.Option("-f|--format", "Format of the generated coverage report.", CommandOptionType.MultipleValue);
+            CommandOption threshold = app.Option("--threshold", "Exits with error if the coverage % is below value.", CommandOptionType.SingleValue);
+            CommandOption thresholdType = app.Option("--threshold-type", "Coverage type to apply the threshold to.", CommandOptionType.MultipleValue);
+            CommandOption filters = app.Option("--exclude", "Filter expressions to exclude specific modules and types.", CommandOptionType.MultipleValue);
+            CommandOption excludes = app.Option("--exclude-by-file", "Glob patterns specifying source files to exclude.", CommandOptionType.MultipleValue);
 
             app.OnExecute(() =>
             {
-                var dotnetTestArgs = new List<string>();
+                if (string.IsNullOrEmpty(module.Value) || string.IsNullOrWhiteSpace(module.Value))
+                    throw new CommandParsingException(app, "No test assembly specified.");
 
-                dotnetTestArgs.Add("test");
+                if (!target.HasValue())
+                    throw new CommandParsingException(app, "Target must be specified.");
 
-                if (project.Value != string.Empty)
-                    dotnetTestArgs.Add(project.Value);
-
-                if (config.HasValue())
-                    dotnetTestArgs.Add($"-c {config.Value()}");
-
-                dotnetTestArgs.Add("/p:CollectCoverage=true");
-
-                if (output.HasValue())
-                    dotnetTestArgs.Add($"/p:CoverletOutput={output.Value()}");
-
-                if (format.HasValue())
-                    dotnetTestArgs.Add($"/p:CoverletOutputFormat={format.Value()}");
+                Coverage coverage = new Coverage(module.Value, filters.Values.ToArray(), excludes.Values.ToArray());
+                coverage.PrepareModules();
 
                 Process process = new Process();
-                process.StartInfo.FileName = "dotnet";
-                process.StartInfo.Arguments = string.Join(" ", dotnetTestArgs);
+                process.StartInfo.FileName = target.Value();
+                process.StartInfo.Arguments = targs.HasValue() ? targs.Value() : string.Empty;
                 process.StartInfo.CreateNoWindow = true;
-
                 process.Start();
                 process.WaitForExit();
 
-                return process.ExitCode;
+                return 0;
             });
 
             try
@@ -58,7 +58,7 @@ namespace coverlet.console
             }
             catch (CommandParsingException ex)
             {
-                Console.WriteLine(ex.Message);
+                logger.LogError(ex.Message);
                 app.ShowHelp();
                 return 1;
             }
