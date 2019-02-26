cedbacb7ac332da2979ff24aa54b9ff317653e45
Zeid Derhally <derhally@gmail.com>
2019-02-26 09:36:37 -0500

    Remove lines to keep original formatting

--- src/coverlet.core/Logging/ILogger.cs
+++ src/coverlet.core/Logging/ILogger.cs
@@ -5,13 +5,9 @@ namespace Coverlet.Core.Logging
     public interface ILogger
     {
         void LogVerbose(string message);
-
         void LogInformation(string message);
-
         void LogWarning(string message);
-
         void LogError(string message);
-
         void LogError(Exception exception);
     }
 }
