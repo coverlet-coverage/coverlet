2201898fe454b89f53265aed4f2c1bd3dce63839
Vishnu Kumar <sasivishnu@gmail.com>
2019-05-06 21:26:20 +0530

    When no line exists, Percent should return default percentage value 100.

--- src/coverlet.core/CoverageDetails.cs
+++ src/coverlet.core/CoverageDetails.cs
@@ -6,6 +6,6 @@ namespace Coverlet.Core
     {
         public double Covered { get; internal set; }
         public int Total { get; internal set; }
-        public double Percent => Total == 0 ? 1D : Math.Floor((Covered / Total) * 10000) / 100;
+        public double Percent => Total == 0 ? 100D : Math.Floor((Covered / Total) * 10000) / 100;
     }
 }
