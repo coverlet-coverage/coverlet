68587972658f958dfc004fe88f6c22d6321d504d
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-05-24 16:57:34 +0200

    updates

--- eng/nightly.ps1
+++ eng/nightly.ps1
@@ -6,6 +6,7 @@ param (
  if (!$apiKey -or !$source)
  {
      Write-Host -ForegroundColor Red Specify apiKey and source
+     exit
  }
 
 $configuration = "Release"
@@ -21,4 +22,4 @@ Write-Host Create Packages
 & $dotnetcli msbuild "$PSScriptRoot\..\build.proj" /t:CreateNuGetPackage /p:Configuration=$configuration # amed build.proj path with future new position under eng
 
 #& $dotnetcli nuget push "$reporoot\build\$configuration\*.nupkg" -k $apiKey -s $source
-& nuget push -Source "TestCoverlet2" -ApiKey AzureDevOps "$reporoot\build\$configuration\*.nupkg" #test with my azure artifacts
+#& nuget push -Source "TestCoverlet2" -ApiKey AzureDevOps "$reporoot\build\$configuration\*.nupkg" #test with my azure artifacts
