cd9f5043285c7d5a71f3a6d7c250fb9781036a9d
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-05-26 17:29:44 +0200

    setup nightly build

---
+++ eng/nightly.ps1
@@ -0,0 +1,19 @@
+param (
+    [string]$apiKey,
+    [string]$source
+ )
+
+if (!$apiKey -or !$source)
+{
+     Write-Host -ForegroundColor Red Specify apiKey and source
+     exit
+}
+
+Write-Host -ForegroundColor Blue Publish with .NET CLI
+& dotnet --info
+
+Write-Host -ForegroundColor Green Create Packages
+& dotnet msbuild "$PSScriptRoot\..\build.proj" /t:CreateNuGetPackage /p:Configuration=Release /p:PublicRelease=false # amend build.proj path if changes
+
+Write-Host -ForegroundColor Green Upload Packages
+& dotnet nuget push "$PSScriptRoot\..\build\Release\*.nupkg" -k $apiKey -s $source
