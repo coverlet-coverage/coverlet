23aeb1c4f3056637cac4325c04155bad30af5a95
DESKTOP-340JJAF\David <muellerdavid4@gmail.com>
2019-09-15 22:08:17 +0200

    extended SystemIoFileAdapter and interface

--- src/coverlet.core/Abstracts/IFileSystem.cs
+++ src/coverlet.core/Abstracts/IFileSystem.cs
@@ -1,7 +1,21 @@
-﻿namespace Coverlet.Core.Abstracts
+﻿using System.IO;
+
+namespace Coverlet.Core.Abstracts
 {
     internal interface IFileSystem
     {
         bool Exists(string path);
+
+        void WriteAllText(string path, string contents);
+
+        string ReadAllText(string path);
+
+        FileStream OpenRead(string path);
+
+        void Copy(string sourceFileName, string destFileName);
+
+        void Delete(string path);
+
+        void AppendAllText(string path, string contents);
     }
 }
--- src/coverlet.core/Helpers/FileSystem.cs
+++ src/coverlet.core/Helpers/FileSystem.cs
@@ -9,5 +9,35 @@ namespace Coverlet.Core.Helpers
         {
             return File.Exists(path);
         }
+
+        public void WriteAllText(string path, string contents)
+        {
+            File.WriteAllText(path, contents);
+        }
+
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public FileStream OpenRead(string path)
+        {
+            return File.OpenRead(path);
+        }
+
+        public void Copy(string sourceFileName, string destFileName)
+        {
+            File.Copy(sourceFileName, destFileName);
+        }
+
+        public void Delete(string path)
+        {
+            File.Delete(path);
+        }
+
+        public void AppendAllText(string path, string contents)
+        {
+            File.AppendAllText(path, contents);
+        }
     }
 }
