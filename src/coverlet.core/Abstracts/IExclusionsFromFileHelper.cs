40bbe5918eaaf7104bad383562c5140a3e88fb04
Thomas Vilhelmsen <thvi@queue-it.com>
2019-11-01 13:28:15 +0100

    Helper to get file names and log fails

---
+++ src/coverlet.core/Abstracts/IExclusionsFromFileHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Coverlet.Core.Abstracts
+{
+    public interface IExclusionsFromFileHelper
+    {
+        string[] ImportExclusionsFromFile(string path);
+    }
+}
---
+++ src/coverlet.core/Helpers/ExclusionsFromFileHelper.cs
@@ -0,0 +1,31 @@
+﻿using Coverlet.Core.Abstracts;
+using System;
+
+namespace coverlet.core.Helpers
+{
+    internal class ExclusionsFromFileHelper : IExclusionsFromFileHelper
+    {
+        private static readonly string[] EmptyResult = new string[0];
+        private readonly IFileSystem _fileSystem;
+        private readonly ILogger _logger;
+
+        public ExclusionsFromFileHelper(IFileSystem fileSystem, ILogger logger)
+        {
+            _fileSystem = fileSystem;
+            _logger = logger;
+        }
+
+        public string[] ImportExclusionsFromFile(string path)
+        {
+            try
+            {
+                return _fileSystem.ReadAllLines(path);
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning($"Failed to parse exclusion file at {path}. Received {e.Message}");
+                return EmptyResult;
+            }
+        }
+    }
+}
