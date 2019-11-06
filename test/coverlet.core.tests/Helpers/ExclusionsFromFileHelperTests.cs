92fb4f26da4f881ef64bd8a6e3dc4270bdfa9d1e
Thomas Vilhelmsen <thvi@queue-it.com>
2019-11-06 08:29:43 +0100

    Added helper  tests

---
+++ test/coverlet.core.tests/Helpers/ExclusionsFromFileHelperTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using coverlet.core.Helpers;
+using Coverlet.Core.Abstracts;
+using Moq;
+using Xunit;
+
+namespace coverlet.core.tests.Helpers
+{
+    public class ExclusionsFromFileHelperTests
+    {
+        private readonly ExclusionsFromFileHelper _sut;
+        private readonly Mock<IFileSystem> _filesSystemMock = new Mock<IFileSystem>();
+        private readonly Mock<ILogger> _loggerMock = new Mock<ILogger>();
+        
+        public ExclusionsFromFileHelperTests()
+        {
+            _sut = new ExclusionsFromFileHelper(_filesSystemMock.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public void TestImportExclusionsFromFile()
+        {
+            var expected = new[] { "../dir1/class1.cs", "../dir2/*.cs", "../dir3/**/*.cs" };
+            _filesSystemMock.Setup(m => m.ReadAllLines(It.IsAny<string>())).Returns(expected);
+            var path = Guid.NewGuid().ToString();
+
+            var actual = _sut.ImportExclusionsFromFile(path);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestImportExclusionsFromFileReturnsEmptyWhenFileSystemThrowsException<T>(T exception) where T: Exception
+        {
+            var expected = new string[0];
+            _filesSystemMock.Setup(m => m.ReadAllLines(It.IsAny<string>())).Throws(exception);
+            var path = Guid.NewGuid().ToString();
+
+            var actual = _sut.ImportExclusionsFromFile(path);
+
+            Assert.Equal(expected, actual);
+        }
+
+        public static TheoryData<Exception> Data => new TheoryData<Exception>
+        {
+            new ArgumentException(),
+            new ArgumentNullException(),
+            new PathTooLongException(),
+            new DirectoryNotFoundException(),
+            new IOException(),
+            new UnauthorizedAccessException(),
+            new FileNotFoundException(),
+            new NotSupportedException(),
+            new System.Security.SecurityException()
+        };
+    }
+}
