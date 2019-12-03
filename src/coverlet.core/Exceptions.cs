3856c76a5627a8d38d2cf2eae9360f05f7d878c5
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-12-03 13:48:39 +0100

    Fix 'Failed to resolve assembly' cecil issue (#625)
    Fix 'Failed to resolve assembly' cecil issue

---
+++ src/coverlet.core/Exceptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Coverlet.Core.Exceptions
+{
+    [Serializable]
+    internal class CoverletException : Exception
+    {
+        public CoverletException() { }
+        public CoverletException(string message) : base(message) { }
+        public CoverletException(string message, System.Exception inner) : base(message, inner) { }
+        protected CoverletException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    internal class CecilAssemblyResolutionException : CoverletException
+    {
+        public CecilAssemblyResolutionException() { }
+        public CecilAssemblyResolutionException(string message) : base(message) { }
+        public CecilAssemblyResolutionException(string message, System.Exception inner) : base(message, inner) { }
+        protected CecilAssemblyResolutionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
