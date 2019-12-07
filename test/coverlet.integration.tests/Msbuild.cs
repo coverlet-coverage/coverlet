92e7b06962a7174f378ffcda65e8b33785007e07
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-12-07 19:07:51 +0100

    add more tests

--- test/coverlet.integration.tests/Msbuild.cs
+++ test/coverlet.integration.tests/Msbuild.cs
@@ -21,8 +21,52 @@ namespace Coverlet.Integration.Tests
             Assert.True(DotnetCli($"test \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\", out string standardOutput, out string standardError), standardOutput);
             Assert.Contains("Test Run Successful.", standardOutput);
             Assert.Contains("| coverletsamplelib.integration.template | 100% | 100%   | 100%   |", standardOutput);
-            Assert.True(File.Exists(Path.Combine(clonedTemplateProject.ProjectRootPath, $"coverage.json")));
+            Assert.True(File.Exists(Path.Combine(clonedTemplateProject.ProjectRootPath, "coverage.json")));
             AssertCoverage(clonedTemplateProject);
+        }
+
+        [Fact]
+        public void TestMsbuild_NoCoverletOutput()
+        {
+            using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
+            Assert.True(DotnetCli($"test \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true", out string standardOutput, out string standardError), standardOutput);
+            Assert.Contains("Test Run Successful.", standardOutput);
+            Assert.Contains("| coverletsamplelib.integration.template | 100% | 100%   | 100%   |", standardOutput);
+            Assert.True(File.Exists(Path.Combine(clonedTemplateProject.ProjectRootPath, "coverage.json")));
+            AssertCoverage(clonedTemplateProject);
+        }
+
+        [Fact]
+        public void TestMsbuild_CoverletOutput_Folder_FileNameWithoutExtension()
+        {
+            using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
+            Assert.True(DotnetCli($"test \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\file", out string standardOutput, out string standardError), standardOutput);
+            Assert.Contains("Test Run Successful.", standardOutput);
+            Assert.Contains("| coverletsamplelib.integration.template | 100% | 100%   | 100%   |", standardOutput);
+            Assert.True(File.Exists(Path.Combine(clonedTemplateProject.ProjectRootPath, "file.json")));
+            AssertCoverage(clonedTemplateProject, "file.json");
+        }
+
+        [Fact]
+        public void TestMsbuild_CoverletOutput_Folder_FileNameExtension()
+        {
+            using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
+            Assert.True(DotnetCli($"test \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\file.ext", out string standardOutput, out string standardError), standardOutput);
+            Assert.Contains("Test Run Successful.", standardOutput);
+            Assert.Contains("| coverletsamplelib.integration.template | 100% | 100%   | 100%   |", standardOutput);
+            Assert.True(File.Exists(Path.Combine(clonedTemplateProject.ProjectRootPath, "file.ext.json")));
+            AssertCoverage(clonedTemplateProject, "file.ext.json");
+        }
+
+        [Fact]
+        public void TestMsbuild_CoverletOutput_Folder_FileNameWithDoubleExtension()
+        {
+            using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
+            Assert.True(DotnetCli($"test \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\file.ext1.ext2", out string standardOutput, out string standardError), standardOutput);
+            Assert.Contains("Test Run Successful.", standardOutput);
+            Assert.Contains("| coverletsamplelib.integration.template | 100% | 100%   | 100%   |", standardOutput);
+            Assert.True(File.Exists(Path.Combine(clonedTemplateProject.ProjectRootPath, "file.ext1.ext2.json")));
+            AssertCoverage(clonedTemplateProject, "file.ext1.ext2.json");
         }
 
         [Fact]
