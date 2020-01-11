ea16e299a00af5b5b73f408496551070d9b8621c
Marco Rossignoli <marco.rossignoli@gmail.com>
2020-01-11 10:40:06 +0100

    allow different test sdk test

--- test/coverlet.integration.tests/BaseTest.cs
+++ test/coverlet.integration.tests/BaseTest.cs
@@ -53,7 +53,7 @@ namespace Coverlet.Integration.Tests
             return manifest.Metadata.Version.OriginalVersion;
         }
 
-        private protected ClonedTemplateProject CloneTemplateProject(bool cleanupOnDispose = true)
+        private protected ClonedTemplateProject CloneTemplateProject(bool cleanupOnDispose = true, string testSDKVersion = "16.4.0")
         {
             DirectoryInfo finalRoot = Directory.CreateDirectory(Guid.NewGuid().ToString("N"));
             foreach (string file in (Directory.GetFiles($"../../../../coverlet.integration.template", "*.cs")
@@ -74,7 +74,7 @@ namespace Coverlet.Integration.Tests
 <Project>
 </Project>");
 
-            AddMicrosoftNETTestSdkRef(finalRoot.FullName);
+            AddMicrosoftNETTestSdkRef(finalRoot.FullName, testSDKVersion);
 
             return new ClonedTemplateProject(finalRoot.FullName, cleanupOnDispose);
         }
@@ -123,7 +123,7 @@ namespace Coverlet.Integration.Tests
             xml.Save(nugetFile);
         }
 
-        private protected void AddMicrosoftNETTestSdkRef(string projectPath)
+        private protected void AddMicrosoftNETTestSdkRef(string projectPath, string version)
         {
             string csproj = Path.Combine(projectPath, "coverlet.integration.template.csproj");
             if (!File.Exists(csproj))
@@ -140,7 +140,7 @@ namespace Coverlet.Integration.Tests
                .Element("ItemGroup")
                .Add(new XElement("PackageReference", new XAttribute("Include", "Microsoft.NET.Test.Sdk"),
                // We use this due to know issue until official release https://github.com/tonerdo/coverlet/blob/master/Documentation/KnowIssues.md
-               new XAttribute("Version", "16.5.0-preview-20191115-01")));
+               new XAttribute("Version", version)));
             xml.Save(csproj);
         }
 
