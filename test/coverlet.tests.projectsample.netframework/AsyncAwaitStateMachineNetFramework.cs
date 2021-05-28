6b6c67a77c5bd8947ae619349f007d5556e3f3d3
David Müller <muellerdavid4@gmail.com>
2021-05-28 10:36:14 +0200

    fix + new sampleproject for tests with targetframework net472

---
+++ test/coverlet.tests.projectsample.netframework/AsyncAwaitStateMachineNetFramework.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace coverlet.tests.projectsample.netframework
+{
+    public class AsyncAwaitStateMachineNetFramework
+    {
+        public async Task AsyncAwait()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
