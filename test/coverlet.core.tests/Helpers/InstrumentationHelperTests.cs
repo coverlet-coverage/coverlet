40258d32c02b877dc8dc3b10a09c52fac38572db
Bert <bert.k@hotmail.com>
2023-08-03 08:42:16 +0200

    use System.CommandLine for coverlet.console (#1486)
    * use System.CommandLine for coverlet.console
    
    * resolve review comments and add DotnetTool tests
    
    * add space between CLI argument (#1499)
    
    matches the same argument further down
    
    * Fix resolving assemblies from frameworks not referenced by coverlet itself (#1449)
    
    * Make tests run on all platforms (#1492)
    
    * Integration test for assembly resolver for mvc.razor (#1502)
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * remove spelling configuration
    
    * use System.CommandLine for coverlet.console
    
    * resolve review comments and add DotnetTool tests
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * Update src/coverlet.console/Program.cs
    
    Co-authored-by: David Müller <muellerdavid4@gmail.com>
    
    * remove spelling configuration
    
    ---------
    
    Co-authored-by: David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
    Co-authored-by: Lukas Grützmacher <44983012+lg2de@users.noreply.github.com>
    Co-authored-by: Cédric Luthi <cedric.luthi@gmail.com>
    Co-authored-by: David Müller <muellerdavid4@gmail.com>

--- test/coverlet.core.tests/Helpers/InstrumentationHelperTests.cs
+++ test/coverlet.core.tests/Helpers/InstrumentationHelperTests.cs
@@ -6,7 +6,6 @@ using System.IO;
 using Xunit;
 using System.Collections.Generic;
 using System.Linq;
-using Castle.Core.Internal;
 using Moq;
 using Coverlet.Core.Abstractions;
 using Coverlet.Core.Enums;
--- test/coverlet.core.tests/Instrumentation/ModuleTrackerTemplateTests.cs
+++ test/coverlet.core.tests/Instrumentation/ModuleTrackerTemplateTests.cs
@@ -1,174 +1,173 @@
 ﻿// Copyright (c) Toni Solarin-Sodara
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Coverlet.Core.Instrumentation;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-
-using Coverlet.Core.Instrumentation;
 using Xunit;
 
 namespace Coverlet.Core.Tests.Instrumentation
 {
-    class TrackerContext : IDisposable
+  class TrackerContext : IDisposable
+  {
+    public TrackerContext()
     {
-        public TrackerContext()
-        {
-            ModuleTrackerTemplate.HitsFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-            ModuleTrackerTemplate.FlushHitFile = true;
-        }
-
-        public void Dispose()
-        {
-            File.Delete(ModuleTrackerTemplate.HitsFilePath);
-            AppDomain.CurrentDomain.ProcessExit -= ModuleTrackerTemplate.UnloadModule;
-            AppDomain.CurrentDomain.DomainUnload -= ModuleTrackerTemplate.UnloadModule;
-        }
+      ModuleTrackerTemplate.HitsFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+      ModuleTrackerTemplate.FlushHitFile = true;
     }
 
-    public class ModuleTrackerTemplateTests : ExternalProcessExecutionTest
+    public void Dispose()
     {
-        private static readonly Task<int> s_success = Task.FromResult(0);
-
-        [Fact]
-        public void HitsFileCorrectlyWritten()
-        {
-            FunctionExecutor.Run(() =>
-            {
-                using var ctx = new TrackerContext();
-                ModuleTrackerTemplate.HitsArray = new[] { 1, 2, 0, 3 };
-                ModuleTrackerTemplate.UnloadModule(null, null);
-
-                int[] expectedHitsArray = new[] { 1, 2, 0, 3 };
-                Assert.Equal(expectedHitsArray, ReadHitsFile());
-
-                return s_success;
-            });
-        }
-
-        [Fact]
-        public void HitsFileWithDifferentNumberOfEntriesCausesExceptionOnUnload()
-        {
-            FunctionExecutor.Run(() =>
-            {
-                using var ctx = new TrackerContext();
-                WriteHitsFile(new[] { 1, 2, 3 });
-                ModuleTrackerTemplate.HitsArray = new[] { 1 };
-                Assert.Throws<InvalidOperationException>(() => ModuleTrackerTemplate.UnloadModule(null, null));
-                return s_success;
-            });
-        }
-
-        [Fact]
-        public void HitsOnMultipleThreadsCorrectlyCounted()
-        {
-            FunctionExecutor.Run(() =>
-            {
-                var threads = new List<Thread>();
-                using var ctx = new TrackerContext();
-                ModuleTrackerTemplate.HitsArray = new[] { 0, 0, 0, 0 };
-                for (int i = 0; i < ModuleTrackerTemplate.HitsArray.Length; ++i)
-                {
-                    var t = new Thread(HitIndex);
-                    threads.Add(t);
-                    t.Start(i);
-                }
-
-                foreach (Thread t in threads)
-                {
-                    t.Join();
-                }
-
-                ModuleTrackerTemplate.UnloadModule(null, null);
-                int[] expectedHitsArray = new[] { 4, 3, 2, 1 };
-                Assert.Equal(expectedHitsArray, ReadHitsFile());
-
-                static void HitIndex(object index)
-                {
-                    int hitIndex = (int)index;
-                    for (int i = 0; i <= hitIndex; ++i)
-                    {
-                        ModuleTrackerTemplate.RecordHit(i);
-                    }
-                }
-
-                return s_success;
-            });
-        }
-
-        [Fact]
-        public void MultipleSequentialUnloadsHaveCorrectTotalData()
-        {
-            FunctionExecutor.Run(() =>
-            {
-                using var ctx = new TrackerContext();
-                ModuleTrackerTemplate.HitsArray = new[] { 0, 3, 2, 1 };
-                ModuleTrackerTemplate.UnloadModule(null, null);
-
-                ModuleTrackerTemplate.HitsArray = new[] { 0, 1, 2, 3 };
-                ModuleTrackerTemplate.UnloadModule(null, null);
-
-                int[] expectedHitsArray = new[] { 0, 4, 4, 4 };
-                Assert.Equal(expectedHitsArray, ReadHitsFile());
-
-                return s_success;
-            });
-        }
-
-        [Fact]
-        public void MutexBlocksMultipleWriters()
-        {
-            FunctionExecutor.Run(async () =>
-            {
-                using var ctx = new TrackerContext();
-                using var mutex = new Mutex(
-                    true, Path.GetFileNameWithoutExtension(ModuleTrackerTemplate.HitsFilePath) + "_Mutex", out bool createdNew);
-                Assert.True(createdNew);
-
-                ModuleTrackerTemplate.HitsArray = new[] { 0, 1, 2, 3 };
-                var unloadTask = Task.Run(() => ModuleTrackerTemplate.UnloadModule(null, null));
-
-                Assert.False(unloadTask.Wait(5));
-
-                WriteHitsFile(new[] { 0, 3, 2, 1 });
-
-                Assert.False(unloadTask.Wait(5));
-
-                mutex.ReleaseMutex();
-                await unloadTask;
-
-                int[] expectedHitsArray = new[] { 0, 4, 4, 4 };
-                Assert.Equal(expectedHitsArray, ReadHitsFile());
-
-                return 0;
-            });
-
-        }
-
-        private static void WriteHitsFile(int[] hitsArray)
-        {
-            using var fs = new FileStream(ModuleTrackerTemplate.HitsFilePath, FileMode.Create);
-            using var bw = new BinaryWriter(fs);
-            bw.Write(hitsArray.Length);
-            foreach (int hitCount in hitsArray)
-            {
-                bw.Write(hitCount);
-            }
-        }
-
-        private static int[] ReadHitsFile()
-        {
-            using var fs = new FileStream(ModuleTrackerTemplate.HitsFilePath, FileMode.Open);
-            using var br = new BinaryReader(fs);
-            int[] hitsArray = new int[br.ReadInt32()];
-            for (int i = 0; i < hitsArray.Length; ++i)
-            {
-                hitsArray[i] = br.ReadInt32();
-            }
-
-            return hitsArray;
-        }
+      File.Delete(ModuleTrackerTemplate.HitsFilePath);
+      AppDomain.CurrentDomain.ProcessExit -= ModuleTrackerTemplate.UnloadModule;
+      AppDomain.CurrentDomain.DomainUnload -= ModuleTrackerTemplate.UnloadModule;
     }
+  }
+
+  public class ModuleTrackerTemplateTests : ExternalProcessExecutionTest
+  {
+    private static readonly Task<int> s_success = Task.FromResult(0);
+
+    [Fact]
+    public void HitsFileCorrectlyWritten()
+    {
+      FunctionExecutor.Run(() =>
+      {
+        using var ctx = new TrackerContext();
+        ModuleTrackerTemplate.HitsArray = new[] { 1, 2, 0, 3 };
+        ModuleTrackerTemplate.UnloadModule(null, null);
+
+        int[] expectedHitsArray = new[] { 1, 2, 0, 3 };
+        Assert.Equal(expectedHitsArray, ReadHitsFile());
+
+        return s_success;
+      });
+    }
+
+    [Fact]
+    public void HitsFileWithDifferentNumberOfEntriesCausesExceptionOnUnload()
+    {
+      FunctionExecutor.Run(() =>
+      {
+        using var ctx = new TrackerContext();
+        WriteHitsFile(new[] { 1, 2, 3 });
+        ModuleTrackerTemplate.HitsArray = new[] { 1 };
+        Assert.Throws<InvalidOperationException>(() => ModuleTrackerTemplate.UnloadModule(null, null));
+        return s_success;
+      });
+    }
+
+    [Fact]
+    public void HitsOnMultipleThreadsCorrectlyCounted()
+    {
+      FunctionExecutor.Run(() =>
+      {
+        var threads = new List<Thread>();
+        using var ctx = new TrackerContext();
+        ModuleTrackerTemplate.HitsArray = new[] { 0, 0, 0, 0 };
+        for (int i = 0; i < ModuleTrackerTemplate.HitsArray.Length; ++i)
+        {
+          var t = new Thread(HitIndex);
+          threads.Add(t);
+          t.Start(i);
+        }
+
+        foreach (Thread t in threads)
+        {
+          t.Join();
+        }
+
+        ModuleTrackerTemplate.UnloadModule(null, null);
+        int[] expectedHitsArray = new[] { 4, 3, 2, 1 };
+        Assert.Equal(expectedHitsArray, ReadHitsFile());
+
+        static void HitIndex(object index)
+        {
+          int hitIndex = (int)index;
+          for (int i = 0; i <= hitIndex; ++i)
+          {
+            ModuleTrackerTemplate.RecordHit(i);
+          }
+        }
+
+        return s_success;
+      });
+    }
+
+    [Fact]
+    public void MultipleSequentialUnloadsHaveCorrectTotalData()
+    {
+      FunctionExecutor.Run(() =>
+      {
+        using var ctx = new TrackerContext();
+        ModuleTrackerTemplate.HitsArray = new[] { 0, 3, 2, 1 };
+        ModuleTrackerTemplate.UnloadModule(null, null);
+
+        ModuleTrackerTemplate.HitsArray = new[] { 0, 1, 2, 3 };
+        ModuleTrackerTemplate.UnloadModule(null, null);
+
+        int[] expectedHitsArray = new[] { 0, 4, 4, 4 };
+        Assert.Equal(expectedHitsArray, ReadHitsFile());
+
+        return s_success;
+      });
+    }
+
+    [Fact]
+    public void MutexBlocksMultipleWriters()
+    {
+      FunctionExecutor.Run(async () =>
+      {
+        using var ctx = new TrackerContext();
+        using var mutex = new Mutex(
+              true, Path.GetFileNameWithoutExtension(ModuleTrackerTemplate.HitsFilePath) + "_Mutex", out bool createdNew);
+        Assert.True(createdNew);
+
+        ModuleTrackerTemplate.HitsArray = new[] { 0, 1, 2, 3 };
+        var unloadTask = Task.Run(() => ModuleTrackerTemplate.UnloadModule(null, null));
+
+        Assert.False(unloadTask.Wait(5));
+
+        WriteHitsFile(new[] { 0, 3, 2, 1 });
+
+        Assert.False(unloadTask.Wait(5));
+
+        mutex.ReleaseMutex();
+        await unloadTask;
+
+        int[] expectedHitsArray = new[] { 0, 4, 4, 4 };
+        Assert.Equal(expectedHitsArray, ReadHitsFile());
+
+        return 0;
+      });
+
+    }
+
+    private static void WriteHitsFile(int[] hitsArray)
+    {
+      using var fs = new FileStream(ModuleTrackerTemplate.HitsFilePath, FileMode.Create);
+      using var bw = new BinaryWriter(fs);
+      bw.Write(hitsArray.Length);
+      foreach (int hitCount in hitsArray)
+      {
+        bw.Write(hitCount);
+      }
+    }
+
+    private static int[] ReadHitsFile()
+    {
+      using var fs = new FileStream(ModuleTrackerTemplate.HitsFilePath, FileMode.Open);
+      using var br = new BinaryReader(fs);
+      int[] hitsArray = new int[br.ReadInt32()];
+      for (int i = 0; i < hitsArray.Length; ++i)
+      {
+        hitsArray[i] = br.ReadInt32();
+      }
+
+      return hitsArray;
+    }
+  }
 }
