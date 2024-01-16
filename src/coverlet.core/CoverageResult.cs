e9a72d5b56ca53c0c2430ff755865ccc7af78d6d
Bert <bert.k@hotmail.com>
2024-01-16 09:25:43 +0100

    add JsonConstructorAttribute to `internal Method()` constructor

--- src/coverlet.core/CoverageResult.cs
+++ src/coverlet.core/CoverageResult.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using Coverlet.Core.Enums;
 using Coverlet.Core.Instrumentation;
 
@@ -22,11 +23,13 @@ namespace Coverlet.Core
 
   internal class Method
   {
+    [JsonConstructor]
     internal Method()
     {
-      Lines = new Lines();
-      Branches = new Branches();
+      Lines = [];
+      Branches = [];
     }
+
     public Lines Lines;
     public Branches Branches;
   }
