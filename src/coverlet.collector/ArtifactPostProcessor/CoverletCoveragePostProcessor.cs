9627e72507fcffe8bf5c92e3dd53514c4acdd078
David Mueller x. <david.x.mueller@getinge.com>
2024-06-03 23:49:32 +0200

    initial implementation

---
+++ src/coverlet.collector/ArtifactPostProcessor/CoverletCoveragePostProcessor.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Toni Solarin-Sodara
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace coverlet.collector.ArtifactPostProcessor
+{
+  public class CoverletCoveragePostProcessor : IDataCollectorAttachmentProcessor
+  {
+    public IEnumerable<Uri>? GetExtensionUris()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<ICollection<AttachmentSet>> ProcessAttachmentSetsAsync(XmlElement configurationElement, ICollection<AttachmentSet> attachments, IProgress<int> progressReporter,
+      IMessageLogger logger, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool SupportsIncrementalProcessing => true;
+  }
+}
