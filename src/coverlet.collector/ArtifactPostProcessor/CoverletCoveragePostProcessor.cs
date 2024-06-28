a6966babc921bb2a2a326feb40945cf242470557
David Müller <muellerdavid4@gmail.com>
2024-06-28 22:13:56 +0200

    prototyping

--- src/coverlet.collector/ArtifactPostProcessor/CoverletCoveragePostProcessor.cs
+++ src/coverlet.collector/ArtifactPostProcessor/CoverletCoveragePostProcessor.cs
@@ -3,28 +3,86 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
+using Coverlet.Collector.Utilities;
+using Coverlet.Core;
+using Coverlet.Core.Abstractions;
+using coverlet.core.Helpers;
+using Coverlet.Core.Reporters;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using Newtonsoft.Json;
 
 namespace coverlet.collector.ArtifactPostProcessor
 {
   public class CoverletCoveragePostProcessor : IDataCollectorAttachmentProcessor
   {
-    public IEnumerable<Uri>? GetExtensionUris()
+    private readonly CoverageResult _coverageResult = new();
+
+    public CoverletCoveragePostProcessor()
     {
-      throw new NotImplementedException();
+      _coverageResult.Modules = new Modules();
+    }
+
+#pragma warning disable CS8632
+    public IEnumerable<Uri>? GetExtensionUris()
+#pragma warning restore CS8632
+    {
+      var uri = new Uri(CoverletConstants.DefaultUri);
+      return new[] { uri };
     }
 
     public Task<ICollection<AttachmentSet>> ProcessAttachmentSetsAsync(XmlElement configurationElement, ICollection<AttachmentSet> attachments, IProgress<int> progressReporter,
       IMessageLogger logger, CancellationToken cancellationToken)
     {
-      throw new NotImplementedException();
+      AttachDebugger();
+
+      foreach (AttachmentSet attachmentSet in attachments)
+      {
+        foreach (UriDataAttachment uriAttachment in attachmentSet.Attachments)
+        {
+          string json = File.ReadAllText(uriAttachment.Uri.LocalPath);
+
+          _coverageResult.Merge(JsonConvert.DeserializeObject<Modules>(json));
+        }
+      }
+
+      (string report, string fileName) report = GetCoverageReport(_coverageResult);
+      string reportDirectory = Path.Combine(Path.GetTempPath(), new Guid().ToString());
+
+      Directory.CreateDirectory(reportDirectory);
+      string filePath = Path.Combine(reportDirectory, report.fileName);
+      File.WriteAllText(filePath, report.report);
+
+      return Task.FromResult(new List<AttachmentSet> {new(new Uri(filePath), report.fileName) } as ICollection<AttachmentSet>);
     }
 
     public bool SupportsIncrementalProcessing => true;
+
+    private void AttachDebugger()
+    {
+      Debugger.Launch();
+      Debugger.Break();
+    }
+
+    private (string report, string fileName) GetCoverageReport(CoverageResult coverageResult)
+    {
+      var reporterFactory = new ReporterFactory("json");
+      IReporter reporter = reporterFactory.CreateReporter();
+
+      try
+      {
+        return (reporter.Report(coverageResult, new DummySourceRootTranslator()), Path.ChangeExtension(CoverletConstants.DefaultFileName, reporter.Extension));
+      }
+      catch (Exception ex)
+      {
+        throw new CoverletDataCollectorException($"{CoverletConstants.DataCollectorName}: Failed to get coverage report", ex);
+      }
+    }
   }
 }
