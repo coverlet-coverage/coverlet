6649c2fe2ecaa6e189454535d9b0374e9ff3ba07
Bert <bert.k@hotmail.com>
2024-07-27 09:32:34 +0200

    use System.Text.Json 8.0.4

--- test/coverlet.tests.utils/TestUtils.cs
+++ test/coverlet.tests.utils/TestUtils.cs
@@ -33,8 +33,8 @@ namespace Coverlet.Tests.Utils
 
     public static string GetAssemblyTargetFramework()
     {
-#if NET7_0
-      return "net7.0";
+#if NET6_0
+      return "net6.0";
 #endif
 #if NET8_0
       return "net8.0";
