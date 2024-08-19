2a72fe55cdcdda88a515ff07790c066e0d36ee28
sunjosep <jojoseph.sun@mail.utoronto.ca>
2024-08-19 17:53:17 -0400

    Possible fix to unit test failure

--- test/coverlet.core.tests/Coverage/CoverageTests.cs
+++ test/coverlet.core.tests/Coverage/CoverageTests.cs
@@ -232,6 +232,7 @@ namespace Coverlet.Core.Tests
       File.Copy(pdb, Path.Combine(directory.FullName, Path.GetFileName(pdb)), true);
 
       var mockInstrumentationHelper = new Mock<IInstrumentationHelper>();
+      mockInstrumentationHelper.Setup(x => x.RestoreOriginalModule(It.IsAny<string>(), It.IsAny<string>()));
 
       var parameters = new CoverageParameters
       {
@@ -266,9 +267,10 @@ namespace Coverlet.Core.Tests
       File.Copy(pdb, Path.Combine(directory.FullName, Path.GetFileName(pdb)), true);
 
       var mockInstrumentationHelper = new Mock<IInstrumentationHelper>();
-        mockInstrumentationHelper
-          .Setup(x => x.SelectModules(It.IsAny<IEnumerable<string>>(), It.IsAny<string[]>(), It.IsAny<string[]>()))
-          .Returns(new List<string>(){"ModuleX"});
+      mockInstrumentationHelper
+        .Setup(x => x.SelectModules(It.IsAny<IEnumerable<string>>(), It.IsAny<string[]>(), It.IsAny<string[]>()))
+        .Returns(new List<string>(){"ModuleX"});
+      mockInstrumentationHelper.Setup(x => x.RestoreOriginalModule(It.IsAny<string>(), It.IsAny<string>()));
 
       var parameters = new CoverageParameters
       {
