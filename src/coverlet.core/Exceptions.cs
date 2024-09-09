2568ed439fcc35ab8999674b1b9c968c20d5d20f
Bert <bert.k@hotmail.com>
2024-09-09 08:41:30 +0200

    remove unused serializable attribute

--- src/coverlet.core/Exceptions.cs
+++ src/coverlet.core/Exceptions.cs
@@ -5,25 +5,17 @@ using System;
 
 namespace Coverlet.Core.Exceptions
 {
-  [Serializable]
   public class CoverletException : Exception
   {
     public CoverletException() { }
     public CoverletException(string message) : base(message) { }
-    public CoverletException(string message, System.Exception inner) : base(message, inner) { }
-    protected CoverletException(
-      System.Runtime.Serialization.SerializationInfo info,
-      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    public CoverletException(string message, Exception inner) : base(message, inner) { }
   }
 
-  [Serializable]
   internal class CecilAssemblyResolutionException : CoverletException
   {
     public CecilAssemblyResolutionException() { }
     public CecilAssemblyResolutionException(string message) : base(message) { }
-    public CecilAssemblyResolutionException(string message, System.Exception inner) : base(message, inner) { }
-    protected CecilAssemblyResolutionException(
-      System.Runtime.Serialization.SerializationInfo info,
-      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    public CecilAssemblyResolutionException(string message, Exception inner) : base(message, inner) { }
   }
 }
