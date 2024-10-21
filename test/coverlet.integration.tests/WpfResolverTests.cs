42dacf08ac81db998723f75282c2bb2095009f7a
Philipp Feigl <philipp.feigl@cyan-it.de>
2024-10-21 10:23:40 +0200

    Fix RuntimeConfigurationReader to support SelfContained builds
    SelfContained builds to not contain the `frameworks` node in the `runtimeconfig.json` file.
    They do however contain a `includedFrameworks` node. Details can be found here: https://github.com/dotnet/sdk/issues/3541
    
    Running tests against a built assembly with the `SelfContained` option does result in the following often discussed error message
    > Unable to instrument module

--- test/coverlet.integration.tests/WpfResolverTests.cs
+++ test/coverlet.integration.tests/WpfResolverTests.cs
@@ -43,5 +43,33 @@ namespace Coverlet.Integration.Tests
           "sample assembly shall be resolved");
       Assert.NotEmpty(assemblies);
     }
+
+    [ConditionalFact]
+    [SkipOnOS(OS.Linux, "WPF only runs on Windows")]
+    [SkipOnOS(OS.MacOS, "WPF only runs on Windows")]
+    public void TestInstrument_NetCoreSharedFrameworkResolver_SelfContained()
+    {
+      string buildConfiguration = TestUtils.GetAssemblyBuildConfiguration().ToString().ToLowerInvariant();
+      string wpfProjectPath = TestUtils.GetTestProjectPath("coverlet.tests.projectsample.wpf8.selfcontained");
+      string testBinaryPath = Path.Combine(TestUtils.GetTestBinaryPath("coverlet.tests.projectsample.wpf8.selfcontained"), buildConfiguration);
+      Assert.True(DotnetCli($"build \"{wpfProjectPath}\"", out string output, out string error));
+      string assemblyLocation = Directory.GetFiles(testBinaryPath, "coverlet.tests.projectsample.wpf8.selfcontained.dll", SearchOption.AllDirectories).First();
+
+      var mockLogger = new Mock<ILogger>();
+      var resolver = new NetCoreSharedFrameworkResolver(assemblyLocation, mockLogger.Object);
+      var compilationLibrary = new CompilationLibrary(
+          "package",
+          "System.Drawing",
+          "0.0.0.0",
+          "sha512-not-relevant",
+          Enumerable.Empty<string>(),
+          Enumerable.Empty<Dependency>(),
+          true);
+
+      var assemblies = new List<string>();
+      Assert.True(resolver.TryResolveAssemblyPaths(compilationLibrary, assemblies),
+          "sample assembly shall be resolved");
+      Assert.NotEmpty(assemblies);
+    }
   }
 }
