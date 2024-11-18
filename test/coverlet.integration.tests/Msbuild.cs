9bc9ef1f80bcd6582542c52f7a1482f183f5a516
Bert <bert.k@hotmail.com>
2024-11-18 16:20:50 +0100

    remove net7.0 target

--- test/coverlet.integration.tests/Msbuild.cs
+++ test/coverlet.integration.tests/Msbuild.cs
@@ -152,7 +152,7 @@ namespace Coverlet.Integration.Tests
     public void Test_MultipleTargetFrameworkReport_NoCoverletOutput()
     {
       using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
-      string[] targetFrameworks = new string[] { "net6.0", "net7.0", "net8.0" };
+      string[] targetFrameworks = new string[] { "net6.0", "net8.0" };
       UpdateProjectTargetFramework(clonedTemplateProject, targetFrameworks);
       DotnetCli($"test -c {_buildConfiguration} \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true", out string standardOutput, out string standardError, clonedTemplateProject.ProjectRootPath!);
       if (!string.IsNullOrEmpty(standardError))
@@ -178,7 +178,7 @@ namespace Coverlet.Integration.Tests
     public void Test_MultipleTargetFrameworkReport_CoverletOutput_Folder()
     {
       using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
-      string[] targetFrameworks = new string[] { "net6.0", "net7.0", "net8.0" };
+      string[] targetFrameworks = new string[] { "net6.0", "net8.0" };
       UpdateProjectTargetFramework(clonedTemplateProject, targetFrameworks);
       bool result = DotnetCli($"test -c {_buildConfiguration} \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\", out string standardOutput, out string standardError, clonedTemplateProject.ProjectRootPath!);
       if (!string.IsNullOrEmpty(standardError))
@@ -206,7 +206,7 @@ namespace Coverlet.Integration.Tests
     public void Test_MultipleTargetFrameworkReport_CoverletOutput_Folder_FileNameWithoutExtension()
     {
       using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
-      string[] targetFrameworks = new string[] { "net6.0", "net7.0", "net8.0" };
+      string[] targetFrameworks = new string[] { "net6.0", "net8.0" };
       UpdateProjectTargetFramework(clonedTemplateProject, targetFrameworks);
       DotnetCli($"test -c {_buildConfiguration} \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\file", out string standardOutput, out string standardError, clonedTemplateProject.ProjectRootPath!);
       if (!string.IsNullOrEmpty(standardError))
@@ -232,7 +232,7 @@ namespace Coverlet.Integration.Tests
     public void Test_MultipleTargetFrameworkReport_CoverletOutput_Folder_FileNameWithExtension_SpecifyFramework()
     {
       using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
-      string[] targetFrameworks = new string[] { "net6.0", "net7.0", "net8.0" };
+      string[] targetFrameworks = new string[] { "net6.0", "net8.0" };
       UpdateProjectTargetFramework(clonedTemplateProject, targetFrameworks);
       Assert.True(clonedTemplateProject.IsMultipleTargetFramework());
       string[] frameworks = clonedTemplateProject.GetTargetFrameworks();
@@ -269,7 +269,7 @@ namespace Coverlet.Integration.Tests
     public void Test_MultipleTargetFrameworkReport_CoverletOutput_Folder_FileNameWithExtension()
     {
       using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
-      string[] targetFrameworks = new string[] { "net6.0", "net7.0", "net8.0" };
+      string[] targetFrameworks = new string[] { "net6.0", "net8.0" };
       UpdateProjectTargetFramework(clonedTemplateProject, targetFrameworks);
       DotnetCli($"test -c {_buildConfiguration} \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\file.ext", out string standardOutput, out string standardError, clonedTemplateProject.ProjectRootPath!);
       if (!string.IsNullOrEmpty(standardError))
@@ -295,7 +295,7 @@ namespace Coverlet.Integration.Tests
     public void Test_MultipleTargetFrameworkReport_CoverletOutput_Folder_FileNameWithDoubleExtension()
     {
       using ClonedTemplateProject clonedTemplateProject = PrepareTemplateProject();
-      string[] targetFrameworks = new string[] { "net6.0", "net7.0", "net8.0" };
+      string[] targetFrameworks = new string[] { "net6.0", "net8.0" };
       UpdateProjectTargetFramework(clonedTemplateProject, targetFrameworks);
       DotnetCli($"test -c {_buildConfiguration} \"{clonedTemplateProject.ProjectRootPath}\" /p:CollectCoverage=true /p:Include=\"[{ClonedTemplateProject.AssemblyName}]*DeepThought\" /p:IncludeTestAssembly=true /p:CoverletOutput=\"{clonedTemplateProject.ProjectRootPath}\"\\file.ext1.ext2", out string standardOutput, out string standardError, clonedTemplateProject.ProjectRootPath!);
       if (!string.IsNullOrEmpty(standardError))
