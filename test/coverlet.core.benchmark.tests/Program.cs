a89879ee6bc5d0e1023f01c80dc108ca39bfec93
Bert <bert.k@hotmail.com>
2025-04-22 10:16:55 +0200

    enable benchmark test for VS

--- test/coverlet.core.benchmark.tests/Program.cs
+++ test/coverlet.core.benchmark.tests/Program.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Toni Solarin-Sodara
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+//using System.Diagnostics.Tracing;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+//using BenchmarkDotNet.Diagnostics.Windows;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Exporters.Csv;
 using BenchmarkDotNet.Exporters.Json;
@@ -10,6 +12,8 @@ using BenchmarkDotNet.Exporters.Xml;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+//using Microsoft.Diagnostics.NETCore.Client;
+//using Microsoft.Diagnostics.Tracing.Parsers;
 
 namespace coverlet.core.benchmark.tests
 {
@@ -18,25 +22,21 @@ namespace coverlet.core.benchmark.tests
 
     public static void Main(string[] args)
     {
-
       var config = DefaultConfig.Instance
             .WithOptions(ConfigOptions.JoinSummary)
             .AddJob(Job
               .ShortRun
               .WithLaunchCount(1)
               .WithToolchain(InProcessNoEmitToolchain.Instance))
-            .AddExporter(CsvExporter.Default)
-            .AddExporter(CsvMeasurementsExporter.Default)
-            .AddExporter(RPlotExporter.Default)
-            .AddExporter(HtmlExporter.Default)
-            .AddExporter(MarkdownExporter.GitHub)
-            .AddExporter(JsonExporter.Default)
-            .AddExporter(XmlExporter.Default)
-            .AddDiagnoser(MemoryDiagnoser.Default)
+            .AddExporter(CsvExporter.Default, CsvMeasurementsExporter.Default, RPlotExporter.Default, HtmlExporter.Default, JsonExporter.Default, MarkdownExporter.GitHub, XmlExporter.Default)
+            .AddDiagnoser(MemoryDiagnoser.Default, ThreadingDiagnoser.Default, ExceptionDiagnoser.Default)
+            //.AddDiagnoser(new InliningDiagnoser(), new EtwProfiler()) // only windows platform, requires elevated privileges
+            //.AddDiagnoser(new EventPipeProfiler(EventPipeProfile.CpuSampling)) // stops collecting results ???
             ;
       var summary = BenchmarkRunner.Run(new[]{
             BenchmarkConverter.TypeToBenchmarks( typeof(CoverageBenchmarks), config),
-            BenchmarkConverter.TypeToBenchmarks( typeof(InstrumenterBenchmarks), config)
+            BenchmarkConverter.TypeToBenchmarks( typeof(InstrumenterBenchmarks), config),
+            BenchmarkConverter.TypeToBenchmarks( typeof(InstrumentationHelperBenchmarks), config),
             });
 
       // Use this to select benchmarks from the console and execute with additional options e.g. 'coverlet.core.benchmark.tests.exe --profiler EP'
