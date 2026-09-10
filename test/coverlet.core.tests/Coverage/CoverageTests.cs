24c01d2369e90aa36b1925346bf83e1b23934ee2
Bert <bert.k@hotmail.com>
2026-09-10 17:43:41 +0200

    remove using System.Linq;

--- test/coverlet.core.tests/Coverage/CoverageTests.cs
+++ test/coverlet.core.tests/Coverage/CoverageTests.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
